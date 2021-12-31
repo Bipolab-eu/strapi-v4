3da5b2d7e5178c9876904aa1d4ad82e98c7a0c8a
Bentagai <bentagai@protonmail.com>
2021-12-31 11:18:02 +0000

    template

--- src/api/obra/controllers/obra.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  obra controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::obra.obra');
--- src/api/obra/services/obra.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * obra service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::obra.obra');
